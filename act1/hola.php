20aed61345090b3105cbb43a13a5e1518f79203b
Ludinox <adrian20509@cbtis75.edu.mx>
2022-10-09 18:31:30 -0500

    Version 1

---
+++ act1/hola.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Hola</title>
+</head>
+<body>
+<article>
+  <h1>What's Going On</h1>
+  <p>Hello World by Adrian Perez</p>
+</article>
+</body>
+</html>
+
+
---
+++ act2/fecha.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Fecha</title>
+</head>
+<body>
+ <link rel="stylesheet" type="text/css" href="style.css">
+<div class="blockquote-wrapper">
+  <div class="blockquote">
+    
+    <h1>
+     La Fecha es: <span style="color:#ffffff"><?php 
+echo date('d-m-y');
+
+?></span> Cada vez falta menos para<span style="color:#ffffff"> Halloween.</span>
+     </h1>
+    <h4>&mdash;Adrian Eduardo Perez Martinez<br>
+Programacion 5PV<br>
+Desarrolla Aplicaciones Web<br><em>PHP is amazing!</em></h4>
+  </div>
+</div>
+</body>
+</html>
+
+
